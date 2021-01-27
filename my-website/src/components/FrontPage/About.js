3e149d3a0b6d19b73cf401aee0843baacb96b12a
acao2002 <xuanan12072002@gmail.com>
2021-01-27 06:01:26 -0500

    finished about

--- my-website/src/components/FrontPage/About.js
+++ my-website/src/components/FrontPage/About.js
@@ -13,12 +13,12 @@ export default function About() {
 
                     <div class="row">
                         <div class="col-lg-4">
-                            <img src="assets/img/profile-img.jpg" class="img-fluid" alt=""/>
+                            <img src="assets/img/profile.jpg" class="img-fluid" alt=""style={{height: '100%', width: '100%',}}/>
                         </div>
                     <div class="col-lg-8 pt-4 pt-lg-0 content">
                         <h3>Computer Science Student &amp; Aspiring Engineer</h3>
                             <p class="font-italic intext">
-                                Hi everyone, I am Andrew Cao. I built this website as a platform for me to share my work, help , and connect with people. I hope you find my projects, blogs, and tutorials interesting and I hope through this you are able to get to know me better.
+                                Hi everyone, I am Andrew Cao. Nice to see you here!
                             </p>
                         <div class="row">
                             <div class="col-lg-6 intext">
@@ -27,23 +27,23 @@ export default function About() {
                                     <li><i class="icofont-rounded-right"></i> <strong>Website:</strong> www.example.com</li>
                                     <li><i class="icofont-rounded-right"></i> <strong>Phone:</strong> +84 961 830 767(Vietnam)
                                     </li>
-                                    <li className ="mr-5">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +857 399 5530(US)
+                                    <li className ="tab">   +857 399 5530(US)
                                     </li>
-                                    <li><i class="icofont-rounded-right"></i> <strong>City:</strong> City : New York, USA</li>
+                                    <li><i class="icofont-rounded-right"></i> <strong>City:</strong> &nbsp;&nbsp;&nbsp;&nbsp; Nashville, TN</li>
                                 </ul>
                         </div>
                         <div class="col-lg-6 intext">
                             <ul>
-                                <li><i class="icofont-rounded-right"></i> <strong>Age:</strong> 30</li>
-                                <li><i class="icofont-rounded-right"></i> <strong>Degree:</strong> Master</li>
-                                <li><i class="icofont-rounded-right"></i> <strong>PhEmailone:</strong> email@example.com</li>
-                                <li><i class="icofont-rounded-right"></i> <strong>Freelance:</strong> Available</li>
+                                <li><i class="icofont-rounded-right"></i> <strong>Age:</strong> 18</li>
+                                <li><i class="icofont-rounded-right"></i> <strong>Education:</strong> Vanderbilt University 2024</li>
+                                <li className ="tab2">   The MacDuffie School 2020</li>
+                                <li className ="tab2">   Tran Dai Nghia 2017 </li>
+                                <li><i class="icofont-rounded-right"></i> <strong>Email:</strong> xuanan12072002@gmail.com</li>
                             </ul>
                         </div>
                             </div>
                                 <p class ="intext">
-                                Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
-                                Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                                    I fell in love with Tech very early in life. As a kid, I spent more time unscrewing my toys and reassembling them than actually playing them. Later on in middle school, I learned of my tech-oriented mindset after exposing myself to coding and IoTs(mostly ardunio) technology. On this website, you can find my work at tech organizations since high school along with my recent side projects in Computer Vision, Machine Learning, Reinforcement Learning, Full Stack web developement, etc. Besides, I would also like to share what I know and my perspectives via blogs and tutorials along with my other hobbies such as music and chess. 
                                 </p>
                             </div>
                         </div>
