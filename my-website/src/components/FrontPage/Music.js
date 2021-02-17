7f24f33769a9724fe5837ad422616bd72d4d02b5
acao2002 <xuanan12072002@gmail.com>
2021-02-17 07:21:32 -0500

    fixed hobbies

--- my-website/src/components/FrontPage/Music.js
+++ my-website/src/components/FrontPage/Music.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import './music.css'
 
 export default class Organizations extends Component {
     render() {
@@ -25,11 +26,11 @@ export default class Organizations extends Component {
                   <p  style ={{color:'lightgray', fontWeight:"bold"}}>What I do</p><p style ={{color:'lightgray'}}>Guitarist/ Learning music production</p>
                   <p  style ={{color:'lightgray', fontWeight:"bold"}}>Channels</p><p style ={{color:'lightgray'}}> Follow me on Youtube and SoundCloud</p>
                   <a href = "https://www.youtube.com/channel/UCr_jeNBhNP9oSqI92sWH8VQ"  target="_blank">
-                    <img class ="mt-4 mx-2"src='/assets/img/sc.png' style = {{width:"25%",height: "20%"}}></img>
+                    <img class =" bt mt-4 mx-2"src='/assets/img/sc.png' style = {{width:"25%",height: "20%"}}></img>
                   </a>
                   <a href = "https://soundcloud.com/xanuchiha2002"  target="_blank">
                    
-                    <img class ="mt-4 mx-2"src='/assets/img/yt.png' style = {{width:"25%",height: "20%"}}></img>
+                    <img class =" bt mt-4 mx-2"src='/assets/img/yt.png' style = {{width:"25%",height: "20%"}}></img>
                   </a>
                   
                   </div>
@@ -54,7 +55,7 @@ export default class Organizations extends Component {
                   <p style ={{color:'lightgray'}}> U13 World school championship 9th runner up(representing Vietnam) 2014</p>
 
                   <p  style ={{color:'lightgray', fontWeight:"bold"}}>Current status</p><p style ={{color:'lightgray'}}> I stopped competing since 2016 and now I only play for fun</p>
-                  <a href = "https://lichess.org/@/andrew223344"  target="_blank">
+                  <a class = "bt" style = {{color:'black', fontWeight:'bold'}} href = "https://lichess.org/@/andrew223344"  target="_blank">
                     Play with me on Lichess
                   </a>
                   
