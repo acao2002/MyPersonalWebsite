bf65b905efea929ce448e81c5e85b947c0182953
acao2002 <xuanan12072002@gmail.com>
2021-02-22 07:51:49 -0500

    project page continue

--- my-website/src/components/Projects/Project.js
+++ my-website/src/components/Projects/Project.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Typed from 'react-typed';
+import './Project.css';
 
 export default class Project extends Component {
     render() {
@@ -27,14 +28,23 @@ export default class Project extends Component {
                         <p>Technology:  <Typed strings={['Ardunio(IoTs) ','Linear Actuators','Web interface','Automation']}typeSpeed={60} backSpeed={70} loop/>
                         </p>
                         <div class="social-links">
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <a href="#" class="ptt"><i class="devicon-github-original" ></i><div >Source code</div></a>
+                            <a href="#" class="ptt"><i class="icofont-newspaper"></i> <div >Paper/Blog</div></a>
+                            <a href="#" class="instagram"><i class="icofont-ui-video-chat"></i><div >Deployed app/Demo </div></a>
+                            <a href="#" class="google-plus"><i class="icofont-key"></i><div > Tutorial </div></a>
                         </div>
                     </div>
+                    <div class="section-title mt-5">
+                        <h3>About</h3>
+                    </div>
+                    <div className = "container" style = {{color: 'white'}}>
+                    Curing paralysis is a question frequently asked. In paralysis recovery, treatment is built around the neuroplasticity function of the brain - the ability to heal and make physiological as the result of interactions with the environment.
+Recovery is normally built around rehabilitative exercises, or physiotherapy, lasting from 3 to 9 months. In physiotherapy, stretching actions are performed on patients’ limbs for prolonged periods of time (1-1.5 hours) to stimulate neuroplasticity. These therapy sessions stimulate neurons to form new connections, which enables patients to regain control of their limbs. 
+
+                    </div>
                 </section>
+
+              
                 
 
             </div>
