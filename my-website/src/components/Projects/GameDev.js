8b6003e89fda0b7d1a4cfbc27156780bcd5bc260
acao2002 <xuanan12072002@gmail.com>
2021-07-24 09:01:24 -0400

    finished game dev

--- my-website/src/components/Projects/GameDev.js
+++ my-website/src/components/Projects/GameDev.js
@@ -30,23 +30,17 @@ export default class GameDev extends Component {
                     <div class="container" data-aos="zoom-in" data-aos-delay="100">
                         <h1>Chocolate Pong</h1>
                         <h3 class = "my-3" style = {{color:'lightgrey'}}>By Andrew Cao</h3>
-                        <p>Technology:  <Typed strings={['Ardunio(IoTs) ','Linear Actuators','Web interface','Automation']}typeSpeed={60} backSpeed={70} loop/>
+                        <p>Technology:  <Typed strings={['Cocos Creator','OOP']}typeSpeed={60} backSpeed={70} loop/>
                         </p>
                         <div class="social-links">
-                            <a href="#" class="ptt"><i class="devicon-github-original" ></i><div >Source code</div></a>
-                            <a href="#" class="ptt"><i class="icofont-newspaper"></i> <div >Paper/Blog</div></a>
-                            <a href="#" class="ptt"><i class="icofont-ui-video-chat"></i><div >Deployed app/Demo </div></a>
-                            <a href="#" class="ptt"><i class="icofont-key"></i><div > Tutorial </div></a>
-                      
+                            <a href="https://github.com/acao2002/Pong" target ="blank" class="ptt"><i class="devicon-github-original" ></i><div >Source code</div></a>
                         </div>
                     </div>
                     <div class="section-title mt-5">
                         <h3>About</h3>
                     </div>
                     <div className = "container" style = {{color: 'white'}}>
-                    Curing paralysis is a question frequently asked. In paralysis recovery, treatment is built around the neuroplasticity function of the brain - the ability to heal and make physiological as the result of interactions with the environment.
-Recovery is normally built around rehabilitative exercises, or physiotherapy, lasting from 3 to 9 months. In physiotherapy, stretching actions are performed on patients’ limbs for prolonged periods of time (1-1.5 hours) to stimulate neuroplasticity. These therapy sessions stimulate neurons to form new connections, which enables patients to regain control of their limbs. 
-
+                    A simple chocolate-themed pong game that I built as an introduction to game development. 
                     </div>
                     <div>
                         <img
@@ -63,13 +57,13 @@ Recovery is normally built around rehabilitative exercises, or physiotherapy, la
                     <div class="container" data-aos="zoom-in" data-aos-delay="100">
                         <h1>Egg game</h1>
                         <h3 class = "my-3" style = {{color:'lightgrey'}}>By Andrew Cao</h3>
-                        <p>Technology:  <Typed strings={['Ardunio(IoTs) ','Linear Actuators','Web interface','Automation']}typeSpeed={60} backSpeed={70} loop/>
+                        <p>Technology:  <Typed strings={['Cocos Creator','Server simulation','Delay handling']}typeSpeed={60} backSpeed={70} loop/>
                         </p>
                         <div class="social-links">
-                            <a href="#" class="ptt"><i class="devicon-github-original" ></i><div >Source code</div></a>
-                            <a href="#" class="ptt"><i class="icofont-newspaper"></i> <div >Paper/Blog</div></a>
-                            <a href="#" class="ptt"><i class="icofont-ui-video-chat"></i><div >Deployed app/Demo </div></a>
-                            <a href="#" class="ptt"><i class="icofont-key"></i><div > Tutorial </div></a>
+                            <a href="https://github.com/acao2002/EggGame" class="ptt" target ="blank"><i class="devicon-github-original" ></i><div >Source code</div></a>
+                           
+                            <a href= "https://sharp-hawking-3302a2.netlify.app/" class="ptt" target ="blank"><i class="icofont-ui-video-chat"></i><div >Deployed app/Demo </div></a>
+                            
                       
                         </div>
                     </div>
@@ -77,8 +71,7 @@ Recovery is normally built around rehabilitative exercises, or physiotherapy, la
                         <h3>About</h3>
                     </div>
                     <div className = "container" style = {{color: 'white'}}>
-                    Curing paralysis is a question frequently asked. In paralysis recovery, treatment is built around the neuroplasticity function of the brain - the ability to heal and make physiological as the result of interactions with the environment.
-Recovery is normally built around rehabilitative exercises, or physiotherapy, lasting from 3 to 9 months. In physiotherapy, stretching actions are performed on patients’ limbs for prolonged periods of time (1-1.5 hours) to stimulate neuroplasticity. These therapy sessions stimulate neurons to form new connections, which enables patients to regain control of their limbs. 
+                    A game where you(player) compete with others to collect as many eggs as possible. The game includes a live leaderboard updating date in realtime, coupled with a server simulation to imitate online gaming environment. Within the server, a delay handling system is implemented to handle lag. Other players follow an AI algorithm to compete against the human player.
 
                     </div>
                 </section>
