27fae6331c7f54a4abed118108df92fb895f9bd9
acao2002 <xuanan12072002@gmail.com>
2021-07-24 02:16:23 -0400

    layout gamedev

--- my-website/src/components/Projects/GameDev.js
+++ my-website/src/components/Projects/GameDev.js
@@ -28,6 +28,39 @@ export default class GameDev extends Component {
                 
                 <section id="hero" class="d-flex flex-column text-center ">
                     <div class="container" data-aos="zoom-in" data-aos-delay="100">
+                        <h1>Chocolate Pong</h1>
+                        <h3 class = "my-3" style = {{color:'lightgrey'}}>By Andrew Cao</h3>
+                        <p>Technology:  <Typed strings={['Ardunio(IoTs) ','Linear Actuators','Web interface','Automation']}typeSpeed={60} backSpeed={70} loop/>
+                        </p>
+                        <div class="social-links">
+                            <a href="#" class="ptt"><i class="devicon-github-original" ></i><div >Source code</div></a>
+                            <a href="#" class="ptt"><i class="icofont-newspaper"></i> <div >Paper/Blog</div></a>
+                            <a href="#" class="ptt"><i class="icofont-ui-video-chat"></i><div >Deployed app/Demo </div></a>
+                            <a href="#" class="ptt"><i class="icofont-key"></i><div > Tutorial </div></a>
+                      
+                        </div>
+                    </div>
+                    <div class="section-title mt-5">
+                        <h3>About</h3>
+                    </div>
+                    <div className = "container" style = {{color: 'white'}}>
+                    Curing paralysis is a question frequently asked. In paralysis recovery, treatment is built around the neuroplasticity function of the brain - the ability to heal and make physiological as the result of interactions with the environment.
+Recovery is normally built around rehabilitative exercises, or physiotherapy, lasting from 3 to 9 months. In physiotherapy, stretching actions are performed on patients’ limbs for prolonged periods of time (1-1.5 hours) to stimulate neuroplasticity. These therapy sessions stimulate neurons to form new connections, which enables patients to regain control of their limbs. 
+
+                    </div>
+                    <div>
+                        <img
+                            class ="text-center mt-4"
+                            src="assets/img/arrow.png"
+                            style = {{width: '10%', height: '60%', }}
+                    />
+                    </div>
+                </section>
+       
+            
+                
+                <section id="hero" class="d-flex flex-column text-center ">
+                    <div class="container" data-aos="zoom-in" data-aos-delay="100">
                         <h1>Egg game</h1>
                         <h3 class = "my-3" style = {{color:'lightgrey'}}>By Andrew Cao</h3>
                         <p>Technology:  <Typed strings={['Ardunio(IoTs) ','Linear Actuators','Web interface','Automation']}typeSpeed={60} backSpeed={70} loop/>
